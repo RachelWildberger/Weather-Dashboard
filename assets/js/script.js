6f92b3e77c7e9ff2b415f523a4d7c6acce4bac65
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2022-12-11 17:23:50 -1000

    added HTML, light CSS styling, Local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 var cityList = document.querySelector("#city-list");
-var cityCountSpan = document.querySelector("#city-count");
 var cityForm = document.querySelector("#city-form");
 var cityInput = document.querySelector("#city-text");
 
@@ -7,7 +6,6 @@ var cities = [];
 
 function renderCities() {
     cityList.innerHTML = "";
-    cityCountSpan.textContent = cities.length;
 
     for (var i = 0; i < cities.length; i++) {
         var city = cities[i];
@@ -17,15 +15,16 @@ function renderCities() {
         li.setAttribute("data-index", i);
 
         var button = document.createElement("button");
-        button.textContent = "Complete ✔️";
-
+        button.textContent = "Search";
+    
         li.appendChild(button);
+
         cityList.appendChild(li);
     }
 }
 
 function init() {
-    // Get stored todos from localStorage
+    // Get stored city from localStorage
     var storedCities = JSON.parse(localStorage.getItem("cities"));
 
     if (storedCities !== null) {
@@ -36,22 +35,38 @@ function init() {
 }
 
 function storeCities() {
-    // Stringify and set key in localStorage to todos array
+    // Stringify and set key in localStorage to cities array
     localStorage.setItem("cities", JSON.stringify(cities));
   }
 
-  cityForm.addEventListener("submit", function(event) {
+  cityForm.addEventListener("click", function(event) {
     event.preventDefault();
   
+    var cityText = cityInput.value.trim();
 
-    if (cityInput === "") {
+    if (cityText === "") {
         return;
       }
 
-    //   cities.push(cityInput);
-    //   cityInput.value = "";
+      cities.push(cityText);
+      cityInput.value = "";
     
-      // Store updated todos in localStorage, re-render the list
+      // Store updated cities in localStorage, re-render the list
       storeCities();
       renderCities();
     });
+
+        // cityList.addEventListener("click", function(event) {
+        // var element = event.target;
+
+        // if (element.matches("button") === true) {
+        //     // Get its data-index value and remove the city element from the list
+        //     var index = element.parentElement.getAttribute("data-index");
+        //     cities.splice(index, 1);
+
+        //     storeCities();
+        //     renderCities();
+        //   }
+        // });
+
+    init()
