0324eaf335b68eb3736b228eaab8d1d962d53f28
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2022-12-13 18:08:08 -1000

    edits to HTML file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,117 @@
-var cityList = document.querySelector(".city-list");
+var cityList = document.querySelector("#city-list");
 var cityForm = document.querySelector("#city-form");
 var cityInput = document.querySelector("#city-text");
 
 var cities = [];
 
 function renderCities() {
-    cityList.innerHTML = "";
+  cityList.innerHTML = "";
 
-    for (var i = 0; i < cities.length; i++) {
-        var city = cities[i];
+  for (var i = 0; i < cities.length; i++) {
+    var city = cities[i];
 
-        var li = document.createElement("li");
-        li.textContent = city;
-        li.setAttribute("data-index", i);
+    var li = document.createElement("li");
+    li.textContent = city;
+    li.setAttribute("data-index", i);
 
-        var button = document.createElement("button");
-        button.textContent = "Search";
-    
-        // li.appendChild(button);
+    var button = document.createElement("button");
+    button.textContent = "Search";
 
-        cityList.appendChild(li);
-    }
+    // li.appendChild(button);
+
+    cityList.appendChild(li);
+  }
 }
 
 function init() {
-    // Get stored city from localStorage
-    var storedCities = JSON.parse(localStorage.getItem("cities"));
+  // Get stored city from localStorage
+  var storedCities = JSON.parse(localStorage.getItem("cities"));
 
-    if (storedCities !== null) {
-        cities = storedCities;
-      }
+  if (storedCities !== null) {
+    cities = storedCities;
+  }
 
-      renderCities();
+  renderCities();
 }
 
 function storeCities() {
-    // Stringify and set key in localStorage to cities array
-    localStorage.setItem("cities", JSON.stringify(cities));
+  // Stringify and set key in localStorage to cities array
+  localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+cityForm.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var cityText = cityInput.value.trim();
+
+  if (cityText === "") {
+    return;
   }
 
-  cityForm.addEventListener("click", function(event) {
-    event.preventDefault();
+  cities.push(cityText);
+  cityInput.value = "";
+
+  // Store updated cities in localStorage, re-render the list
+  storeCities();
+  renderCities();
+});
+
+// cityList.addEventListener("click", function(event) {
+// var element = event.target;
+
+// if (element.matches("button") === true) {
+//     // Get its data-index value and remove the city element from the list
+//     var index = element.parentElement.getAttribute("data-index");
+//     cities.splice(index, 1);
+
+//     storeCities();
+//     renderCities();
+//   }
+// });
+
+init()
+
+// todays highlights
+
+
+var lon;
+var lat;
+var todayTemp = document.querySelector("#today-temp");
+var todayWind = document.querySelector("#today-wind");
+var todayHumidity = document.querySelector("#today-humidity");
+var iconEl = document.querySelector(".icon");
+
+var APIKey = "a34bef93ccd653626bea99311ab551c5";
+
+function getWeatherData(cityName) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + this.APIKey;
+
+      fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+
+}
   
-    var cityText = cityInput.value.trim();
 
-    if (cityText === "") {
-        return;
-      }
 
-      cities.push(cityText);
-      cityInput.value = "";
+
+
+  // if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(position); {
+  //     console.log(position);
+
+  //     lon = position.coords.longitude;
+  //     lat = position.coords.latitude;
+
+  //     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + lat + lon + "appid=" + this.APIKey;
+
+
+
     
-      // Store updated cities in localStorage, re-render the list
-      storeCities();
-      renderCities();
-    });
-
-        // cityList.addEventListener("click", function(event) {
-        // var element = event.target;
-
-        // if (element.matches("button") === true) {
-        //     // Get its data-index value and remove the city element from the list
-        //     var index = element.parentElement.getAttribute("data-index");
-        //     cities.splice(index, 1);
-
-        //     storeCities();
-        //     renderCities();
-        //   }
-        // });
-
-    init()
+  //       }
+  //     }
+  //   }
