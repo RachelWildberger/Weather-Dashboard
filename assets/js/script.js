5c71af87321454b33f12f825fb75c9ce94a231b9
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2022-12-17 18:21:53 -1000

    edited city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,48 @@
-var cityList = document.querySelector("#city-list");
+var cityHistory = document.querySelector("#city-history");
 var cityForm = document.querySelector("#city-form");
 var cityInput = document.querySelector("#city-text");
+var clearHistory = document.querySelector("#clear-button");
+var submitBtn = document.querySelector("#submit-button")
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+//  get cities from local storage
+submitBtn.addEventListener("click", function(){
+  var search = cityInput.value;
+  getWeatherData(search);
+  searchHistory.push(search);
+  localStorage.setItem("search", JSON.stringify(searchHistory));
+  renderSearchHistory();
+})
 
 
-var cities = [];
+//  clear history button
+clearHistory.addEventListener("click", function (){
+  localStorage.clear();
+  searchHistory = [];
+  renderSearchHistory();
+  console.log("click");
+})
 
-function renderCities() {
-  cityList.innerHTML = "";
 
-  for (var i = 0; i < cities.length; i++) {
-    var city = cities[i];
 
-    var li = document.createElement("li");
-    li.textContent = city;
-    li.setAttribute("data-index", i);
-
-    var button = document.createElement("button");
-    button.textContent = "Search";
-
-    // li.appendChild(button);
-
-    cityList.appendChild(li);
-  }
+function renderSearchHistory() {
+    cityHistory.innerHTML = "";
+    for (var i = 0; i < searchHistory.length; i++) {
+      var historyItem = document.createElement("input");
+      historyItem.setAttribute("type", "text");
+      historyItem.setAttribute("readonly", true);
+      historyItem.setAttribute("class", "form-control d-block bg-white");
+      historyItem.setAttribute("click", function (){
+        getWeatherData(historyItem.value);
+      });
+      cityHistory.append(historyItem);
+    }
+}
+renderSearchHistory();
+    if (searchHistory.length > 0){
+      getWeatherData(searchHistory[searchHistory.length - 1]);
 }
 
-function init() {
-  // Get stored city from localStorage
-  var storedCities = JSON.parse(localStorage.getItem("cities"));
-
-  if (storedCities !== null) {
-    cities = storedCities;
-  }
-
-  renderCities();
-}
-
-function storeCities() {
-  // Stringify and set key in localStorage to cities array
-  localStorage.setItem("cities", JSON.stringify(cities));
-}
-
-cityForm.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  var cityText = cityInput.value.trim();
-
-  if (cityText === "") {
-    return;
-  }
-
-  cities.push(cityText);
-  cityInput.value = "";
-
-  // Store updated cities in localStorage, re-render the list
-  storeCities();
-  renderCities();
-});
-
-cityList.addEventListener("click", function(event) {
-var element = event.target;
-
-if (element.matches("button") === true) {
-    // Get its data-index value and remove the city element from the list
-    var index = element.parentElement.getAttribute("data-index");
-    cities.splice(index, 1);
-
-    storeCities();
-    renderCities();
-  }
-});
-
-init()
-
-
-var cityName="";
-var weatherCity = document.querySelector("#weather-city");
 
 
 
@@ -91,16 +59,17 @@ function displayWeather(event) {
 
 
 
-function getWeatherData() {
+
+function getWeatherData(cityName) {
   // var {lat} = cityName;
   // var {lon} = cityName;
   // var cityName = location.name;
 
-  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=33.44&lon=-94.04&units=imperial&appid=${APIKey}`;
-
+  // var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
 
   // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=33.44&lon=-94.04&units=imperial&appid=${APIKey}`;
   
   // var lat = cityName.coord.lat;
   // var lon = cityName.coord.lon;
@@ -111,17 +80,15 @@ function getWeatherData() {
       })
       .then(function (data) {
         console.log(data);
+      });
+  }
 
-        for (var i = 0; i < data.length; +ii) {
-          
-        }
-      })
-}
+  getWeatherData();
 
-var todayHighlights = document.querySelector("#today-highlights");
-var todayTemp = document.querySelector("#today-temp");
-var todayWind = document.querySelector("#today-wind");
-var todayHumidity = document.querySelector("#today-humidity");
+// var todayHighlights = document.querySelector("#today-highlights");
+// var todayTemp = document.querySelector("#today-temp");
+// var todayWind = document.querySelector("#today-wind");
+// var todayHumidity = document.querySelector("#today-humidity");
 
 
 
@@ -147,3 +114,5 @@ var todayHumidity = document.querySelector("#today-humidity");
 //   }).catch(function(error){
 //     console.log(error);
 //   })
+
+
