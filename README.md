dd703f42f2129a8b1c4bed64cea2cb16c940e632
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2022-12-19 14:32:03 -1000

    accessed API key and 5 day forcast

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Weather-Dashboard
+
+## Project Scope
+
+Create a weather application using a third-party API and Bootstrap CSS framework. This application runs in the browser and features dynamically updated HTML and CSS. Using the 5 Day Weather Forecast, https://openweathermap.org/forecast5, to retrieve temperature, wind speed, and humidity for selected location.
+
+The weather dashboard has a form input where the user can search desired city. Once presented with the city, the application posts current conditions and a five day forecast for that city. The searched cities stay in local storage and are added to the search history.
+
+The weather dashbord includes; city name, date, icon representation of weather conditions, the temperature, the humidity, and the the wind speed.
+
+## Installation
+
+Visiting // views the weather dashboard. 
+
+## Usage
+
+
+![Weather Dashboard Wireframe](assets/images/Weather-Dashboard-wireframe.png)
+
+## Credits
+
+Open Weather App - https://openweathermap.org/
+
+Bootstrap - https://getbootstrap.com/
+
+If you followed tutorials, include links to those here as well.
+
+
+## License
+
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,87 @@ var cityForm = document.querySelector("#city-form");
 var cityInput = document.querySelector("#city-text");
 var clearHistory = document.querySelector("#clear-button");
 var submitBtn = document.querySelector("#submit-button")
+
+var todayDate = document.getElementById("today-date");
+var todayTemp = document.getElementById("today-temp");
+var todayWind = document.getElementById("today-wind");
+var todayHumid = document.getElementById("today-humidity");
+var icon = document.getElementById("current-icon");
+var cityName = document.getElementById("city-name");
+
+
+
+
+var day0Temp = document.getElementById("day0-temp");
+
+
+
+var APIKey = "a34bef93ccd653626bea99311ab551c5";
+
+
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
+function getLatLon(name) {
+
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&units=imperial&appid=" + APIKey;
+
+  fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    getWeatherData(lat, lon);
+    console.log(data);
+  });
+}
+
+function getWeatherData(lat, lon) {
+
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
+
+      fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var days = [];
+         for (var i=0; i < data.list.length; i++) {
+          if (data.list[i].dt_txt.slice(11, 13) == "12") {
+            days.push(data.list[i]);
+          }
+         }
+
+        // console.log(days);
+        // console.log(data);
+      });
+  }
+  var displayTodayWeather = function (data) {
+    
+    var cityName = data.name;
+    var icon = data.weather.icon;
+    var todayTemp = data.main.temp;
+    var todayWind = data.wind.speed
+  
+  console.log(cityName, icon, todayTemp, todayWind);
+  cityName.innerText = cityInput;
+  
+  }
+
+
+
+
+  // var day1Temp = document.querySelector("#day1-temp");
+  // var day2Temp = document.querySelector("#day2-temp");
+  
+
+
 //  get cities from local storage
-submitBtn.addEventListener("click", function(){
-  var search = cityInput.value;
-  getWeatherData(search);
-  searchHistory.push(search);
+  submitBtn.addEventListener("click", function (){
+  var searchTerm = cityInput.value;
+  getLatLon(searchTerm);
+  searchHistory.push(searchTerm);
   localStorage.setItem("search", JSON.stringify(searchHistory));
   renderSearchHistory();
 })
@@ -20,7 +94,6 @@ clearHistory.addEventListener("click", function (){
   localStorage.clear();
   searchHistory = [];
   renderSearchHistory();
-  console.log("click");
 })
 
 
@@ -33,21 +106,18 @@ function renderSearchHistory() {
       historyItem.setAttribute("readonly", true);
       historyItem.setAttribute("class", "form-control d-block bg-white");
       historyItem.setAttribute("click", function (){
-        getWeatherData(historyItem.value);
+        // getWeatherData(historyItem.value);
       });
       cityHistory.append(historyItem);
     }
 }
-renderSearchHistory();
-    if (searchHistory.length > 0){
-      getWeatherData(searchHistory[searchHistory.length - 1]);
-}
 
 
 
 
-// Todays highlights
-var APIKey = "a34bef93ccd653626bea99311ab551c5";
+
+// Get current request from API
+
 
 function displayWeather(event) {
   event.preventDefault();
@@ -56,63 +126,3 @@ function displayWeather(event) {
     getWeatherData(weatherCity);
   }
 }
-
-
-
-
-function getWeatherData(cityName) {
-  // var {lat} = cityName;
-  // var {lon} = cityName;
-  // var cityName = location.name;
-
-  // var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
-
-  // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-
-  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=33.44&lon=-94.04&units=imperial&appid=${APIKey}`;
-  
-  // var lat = cityName.coord.lat;
-  // var lon = cityName.coord.lon;
-
-      fetch(queryURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
-  }
-
-  getWeatherData();
-
-// var todayHighlights = document.querySelector("#today-highlights");
-// var todayTemp = document.querySelector("#today-temp");
-// var todayWind = document.querySelector("#today-wind");
-// var todayHumidity = document.querySelector("#today-humidity");
-
-
-
-
-
-// var changeToImperial = `https://api.openweathermap.org//data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
-
-// function getCoordinates(search) {
-
-//   var coordsUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${APIKey}`;
-
-//   fetch(coordsUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     if (!data[0]) {
-//       alert("Error location not found!");
-//     } else {
-//       getWeatherData(data[0]);
-//       console.log(data[0]);
-//     }
-//   }).catch(function(error){
-//     console.log(error);
-//   })
-
-
